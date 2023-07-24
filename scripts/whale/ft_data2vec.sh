9054a911fdc1be8aa88dc465f5a65316e9d54b5e
dohe0342 <dohe1070@gmail.com>
2023-07-24 18:58:12 +0900

    from local

--- scripts/whale/ft_data2vec.sh
+++ scripts/whale/ft_data2vec.sh
@@ -9,4 +9,4 @@ fairseq-hydra-train \
 	model.w2v_path=/home/work/workspace/models/data2vec_model/audio_base_ls.pt \
 	criterion._name=interctc \
 	checkpoint.save_dir=/home/work/workspace/fairseq/scripts/whale/outputs/$1 \
-	checkpoint.best_checkpoint_metric=wer
+	checkpoint.best_checkpoint_metric=wer_11
