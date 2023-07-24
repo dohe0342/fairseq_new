71e5a99aa02a4fdd89c140eab7c389daec765ab3
dohe0342 <kimdohe1070@gmail.com>
2023-07-24 16:50:56 +0900

    from local

--- fairseq/criterions/ctc.py
+++ fairseq/criterions/ctc.py
@@ -330,6 +330,7 @@ class InterCtcCriterion(CtcCriterion):
     def __init__(
         self, cfg: CtcCriterionConfig, task: FairseqTask, rdrop_alpha: int = 0.0
     ):
+        super().__init__(cfg, task, rdrop_alpha)
         CtcCriterion.super().__init__(task)
         self.blank_idx = (
             task.target_dictionary.index(task.blank_symbol)
