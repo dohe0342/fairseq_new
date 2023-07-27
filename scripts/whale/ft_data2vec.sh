13f577b4434ff788937a4d09b38d635f059519e5
dohe0342 <kimdohe1070@gmail.com>
2023-07-27 17:02:36 +0900

    from local

--- scripts/whale/ft_data2vec.sh
+++ scripts/whale/ft_data2vec.sh
@@ -19,11 +19,11 @@ fairseq-hydra-train \
 	--config-dir /home/work/workspace/fairseq/examples/wav2vec/config/finetuning \
 	--config-name base_100h \
 	common.user_dir=examples/data2vec \
-	common.tensorboard_logdir=/home/work/workspace/fairseq/scripts/whale/outputs/$1 \
+	common.tensorboard_logdir=/home/work/workspace/fairseq/scripts/whale/outputs/$2 \
 	task.data=/home/work/workspace/LibriSpeech/manifests \
 	task.normalize=true \
 	model.w2v_path=/home/work/workspace/models/data2vec_model/audio_base_ls.pt \
-	checkpoint.save_dir=/home/work/workspace/fairseq/scripts/whale/outputs/$1 \
+	checkpoint.save_dir=/home/work/workspace/fairseq/scripts/whale/outputs/$2 \
 	dataset.train_subset=cv_train \
 	dataset.valid_subset=cv_dev \
 	optimization.max_update=20000 \
