1a20a307daeaa374232ae57a48db8c10dc571168
dohe0342 <kimdohe1070@gmail.com>
2023-07-27 16:45:52 +0900

    from local

---
+++ scripts/whale/ft_data2vec_prompt.sh
@@ -0,0 +1,14 @@
+git pull
+
+fairseq-hydra-train \
+	--config-dir /home/work/workspace/fairseq/examples/wav2vec/config/finetuning \
+	--config-name base_100h \
+	common.user_dir=examples/data2vec \
+	common.tensorboard_logdir=/home/work/workspace/fairseq/scripts/whale/outputs/$1 \
+	task.data=/home/work/workspace/LibriSpeech/manifests \
+	task.normalize=true \
+	model.w2v_path=/home/work/workspace/models/data2vec_model/audio_base_ls.pt \
+	checkpoint.save_dir=/home/work/workspace/fairseq/scripts/whale/outputs/$1 \
+	dataset.train_subset=ted2_train \
+	dataset.valid_subset=ted2_dev \
+	+task.min_sample_size=32000
