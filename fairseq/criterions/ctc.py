731117c3e90d1c787c078b6c26d40b37741f957f
dohe0342 <kimdohe1070@gmail.com>
2023-07-29 12:52:11 +0900

    from local

--- fairseq/criterions/ctc.py
+++ fairseq/criterions/ctc.py
@@ -581,7 +581,7 @@ class PromptCtcCriterion(CtcCriterion):
         self, cfg: CtcCriterionConfig, task: FairseqTask, rdrop_alpha: int = 0.0
     ):
         super().__init__(cfg, task, rdrop_alpha)
-        self.prompt = torch.nn.Parameter(torch.rand(50, 512))
+        self.prompt = torch.nn.Parameter(torch.rand(50, 512)/10.)
         
     def forward(self, model, sample, reduce=True, **kwargs):
         sample['net_input']['prompt'] = self.prompt
