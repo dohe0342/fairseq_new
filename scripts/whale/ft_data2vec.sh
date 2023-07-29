f42be1ca1169c45b7d3227db6a75a76af66ee5fa
dohe0342 <kimdohe1070@gmail.com>
2023-07-29 12:50:34 +0900

    from local

--- scripts/whale/ft_data2vec.sh
+++ scripts/whale/ft_data2vec.sh
@@ -16,7 +16,7 @@ fairseq-hydra-train \
 	criterion._name=prompt \
 	+task.min_sample_size=16000
 
-#: <<'END'
+: <<'END'
 fairseq-hydra-train \
 	--config-dir /home/work/workspace/fairseq/examples/wav2vec/config/finetuning \
 	--config-name base_100h \
@@ -33,4 +33,4 @@ fairseq-hydra-train \
 	criterion._name=prompt \
 	+task.min_sample_size=16000
 #model.freeze_finetune_updates=20000 \
-#END
+END
