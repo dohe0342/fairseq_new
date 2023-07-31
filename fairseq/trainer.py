782f7fb036fe31db029184071580140f08dbebbf
dohe0342 <kimdohe1070@gmail.com>
2023-07-31 21:18:47 +0900

    from local

--- fairseq/trainer.py
+++ fairseq/trainer.py
@@ -288,12 +288,13 @@ class Trainer(object):
 
     def _build_optimizer(self):
         for n, p in self.model.named_parameters():
-            #if 'bias' in n or 'norm' in n:
-            #    logger.info(f"{n} requires grad = True")
-            #    p.requires_grad = True
+            if 'bias' in n or 'norm' in n:
+                logger.info(f"{n} requires grad = True")
+                p.requires_grad = True
             else:
                 p.requires_grad = False
             #p.requires_grad = False
+            #if 'q_proj' in n:
 
         if (
             self.cfg.optimization.debug_param_names
