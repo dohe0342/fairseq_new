d2ad9b6325e8544a76b8e61b58dd5ad608667da2
dohe0342 <kimdohe1070@gmail.com>
2023-07-31 21:49:23 +0900

    from local

--- scripts/whale/ft_data2vec.sh
+++ scripts/whale/ft_data2vec.sh
@@ -5,6 +5,7 @@ fairseq-hydra-train \
 	--config-name base_100h \
 	common.user_dir=examples/data2vec \
 	common.tensorboard_logdir=/home/work/workspace/fairseq/scripts/whale/outputs/$1 \
+	common.log_file=/home/work/workspace/fairseq/scripts/whale/outputs/$1 \
 	task.data=/home/work/workspace/LibriSpeech/manifests \
 	task.normalize=true \
 	model.w2v_path=/home/work/workspace/models/data2vec_model/audio_base_ls.pt \
@@ -14,7 +15,7 @@ fairseq-hydra-train \
 	optimization.max_update=80000 \
 	optimization.lr=[0.00003] \
 	criterion._name=ctc \
-	+task.min_sample_size=16000
+	+task.min_sample_size=16000 \
 
 : <<'END'
 fairseq-hydra-train \
