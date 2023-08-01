d079c39f7b9312bd49225703ce82e4e660094f7c
dohe0342 <kimdohe1070@gmail.com>
2023-08-01 16:55:56 +0900

    from local

--- fairseq/models/wav2vec/wav2vec2.py
+++ fairseq/models/wav2vec/wav2vec2.py
@@ -1389,6 +1389,32 @@ class TransformerSentenceEncoderLayer(nn.Module):
                 x = self.dropout3(x)
                 x = residual + x
                 x = self.final_layer_norm(x)
+            
+            else:
+                x, attn = self.self_attn(
+                    query=x,
+                    key=x,
+                    value=x,
+                    key_padding_mask=self_attn_padding_mask,
+                    need_weights=False,
+                )
+
+                x = self.dropout1(x)
+                x = residual + x
+
+                x = self.self_attn_layer_norm(x)
+
+                residual = x
+                x = self.activation_fn(self.fc1(x))
+                x = self.dropout2(x)
+                x = self.fc2(x)
+
+                layer_result = x
+
+                x = self.dropout3(x)
+                x = residual + x
+                x = self.final_layer_norm(x)
+
 
         return x, (attn, layer_result)
 
