4ad83d60643822fb33d7a84ee58bc662daee88be
dohe0342 <kimdohe1070@gmail.com>
2023-08-08 14:40:33 +0900

    from local

--- fairseq/models/wav2vec/wav2vec2.py
+++ fairseq/models/wav2vec/wav2vec2.py
@@ -1498,12 +1498,11 @@ class AdapterFast(nn.Module):
             fan_in, _ = nn.init._calculate_fan_in_and_fan_out(self.W_b[ii])
             bound = 1 / math.sqrt(fan_in) if fan_in > 0 else 0
             nn.init.uniform_(self.b_b[ii], -bound, bound)
-        
-        for ii in range(self.adapter_num):
-            self.W_a[ii] /= 10.
-            self.W_b[ii] /= 10.
-            self.b_a /= 10.
-            self.b_b /= 10.
+
+            self.W_a[ii].data /= 10.
+            self.W_b[ii].data /= 10.
+            self.b_a.data /= 10.
+            self.b_b.data /= 10.
         
         nn.init.ones_(self.ln_W)
         nn.init.zeros_(self.ln_b)
