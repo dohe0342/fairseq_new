14797d94d5329c701aad31cae4cff0447b675d4d
dohe0342 <kimdohe1070@gmail.com>
2023-08-08 13:06:30 +0900

    from local

---
+++ scripts/whale/ft_data2vec_adp.sh
@@ -0,0 +1,35 @@
+fairseq-hydra-train \
+	--config-dir /home/work/workspace/fairseq/examples/wav2vec/config/finetuning \
+	--config-name base_100h \
+	common.user_dir=examples/data2vec \
+	common.tensorboard_logdir=/home/work/workspace/fairseq/scripts/whale/outputs/$1 \
+	common.log_file=/home/work/workspace/fairseq/scripts/whale/outputs/$1.log \
+	task.data=/home/work/workspace/LibriSpeech/manifests \
+	task.normalize=true \
+	model.w2v_path=/home/work/workspace/models/data2vec_model/audio_base_ls.pt \
+	checkpoint.save_dir=/home/work/workspace/fairseq/scripts/whale/outputs/$1 \
+	dataset.train_subset=ted2_train \
+	dataset.valid_subset=ted2_dev \
+	optimization.max_update=80000 \
+	optimization.lr=[0.0003] \
+	criterion._name=prompt \
+	+task.min_sample_size=16000 \
+
+: <<'END'
+fairseq-hydra-train \
+	--config-dir /home/work/workspace/fairseq/examples/wav2vec/config/finetuning \
+	--config-name base_100h \
+	common.user_dir=examples/data2vec \
+	common.tensorboard_logdir=/home/work/workspace/fairseq/scripts/whale/outputs/$2 \
+	task.data=/home/work/workspace/LibriSpeech/manifests \
+	task.normalize=true \
+	model.w2v_path=/home/work/workspace/models/data2vec_model/audio_base_ls.pt \
+	checkpoint.save_dir=/home/work/workspace/fairseq/scripts/whale/outputs/$2 \
+	dataset.train_subset=cv_train \
+	dataset.valid_subset=cv_dev \
+	optimization.max_update=20000 \
+	optimization.lr=[0.00003] \
+	criterion._name=prompt \
+	+task.min_sample_size=16000
+#model.freeze_finetune_updates=20000 \
+END
