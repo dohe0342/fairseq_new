da795e6b4ac80e9313e30d4463fb3802f586d2d0
dohe0342 <kimdohe1070@gmail.com>
2023-08-08 14:31:44 +0900

    from local

--- fairseq/models/wav2vec/wav2vec2.py
+++ fairseq/models/wav2vec/wav2vec2.py
@@ -1572,6 +1572,9 @@ class TransformerSentenceEncoderWithAdapterLayer(TransformerSentenceEncoderLayer
         )
         #assert corpus_key is not None
         #assert len(set(corpus_key)) == 1, f"corpus_key items are not same {corpus_key}"
+        print('*'*20)
+        print(corpus_key)
+        print('*'*20)
         y = self.adapter_layer(x, corpus_key[0])
         x = x + y
         return x, (attn, layer_result)
