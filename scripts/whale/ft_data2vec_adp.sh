b6e6ac3f75e459f1ca786b9dae3e66be2f257d14
dohe0342 <kimdohe1070@gmail.com>
2023-08-10 02:28:19 +0900

    from local

--- scripts/whale/ft_data2vec_adp.sh
+++ scripts/whale/ft_data2vec_adp.sh
@@ -15,7 +15,8 @@ fairseq-hydra-train \
 	criterion._name=ctc \
 	+task.min_sample_size=16000 \
 	+model.layer_type=trf_adp \
-	+model.adp_num=1 
+	+model.adp_num=1 \
+	+model.adp_trf_idx=3:4:5:6:7:8:9:10:11
 
 : <<'END'
 fairseq-hydra-train \
