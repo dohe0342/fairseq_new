76527d8c353d208503260131f782057332bdbb50
dohe0342 <kimdohe1070@gmail.com>
2023-08-10 02:33:34 +0900

    from local

--- scripts/whale/ft_data2vec_adp.sh
+++ scripts/whale/ft_data2vec_adp.sh
@@ -3,12 +3,12 @@ for i in {1..3}; do
 		--config-dir /home/work/workspace/fairseq/examples/wav2vec/config/finetuning \
 		--config-name base_100h \
 		common.user_dir=examples/data2vec \
-		common.tensorboard_logdir=/home/work/workspace/fairseq/scripts/whale/outputs/$1 \
-		common.log_file=/home/work/workspace/fairseq/scripts/whale/outputs/$1.log \
+		common.tensorboard_logdir=/home/work/workspace/fairseq/scripts/whale/outputs/$1_$i \
+		common.log_file=/home/work/workspace/fairseq/scripts/whale/outputs/$1_$i.log \
 		task.data=/home/work/workspace/LibriSpeech/manifests \
 		task.normalize=true \
 		model.w2v_path=/home/work/workspace/models/data2vec_model/audio_base_ls.pt \
-		checkpoint.save_dir=/home/work/workspace/fairseq/scripts/whale/outputs/$1 \
+		checkpoint.save_dir=/home/work/workspace/fairseq/scripts/whale/outputs/$1_$i \
 		dataset.train_subset=ted2_train \
 		dataset.valid_subset=ted2_dev \
 		optimization.max_update=80000 \
