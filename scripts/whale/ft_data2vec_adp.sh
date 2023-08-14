3990640e86b64ec13396b88f243cdd88ffcb3ce4
dohe0342 <kimdohe1070@gmail.com>
2023-08-14 14:48:46 +0900

    from local

--- scripts/whale/ft_data2vec_adp.sh
+++ scripts/whale/ft_data2vec_adp.sh
@@ -52,8 +52,7 @@ for i in {11..1}; do
 		checkpoint.save_dir=/home/work/workspace/fairseq/scripts/whale/outputs/$1_last_$i \
 		dataset.train_subset=ted2_train \
 		dataset.valid_subset=ted2_dev \
-		optimization.max_update=0 \
-		optimization.max_epoch=30 \
+		optimization.max_update=20000 \
 		optimization.lr=[0.0001] \
 		criterion._name=ctc \
 		+task.min_sample_size=16000 \
