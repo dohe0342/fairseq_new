8ebe6629cec2c278580147a5e499da2a0fdd9c11
dohe0342 <kimdohe1070@gmail.com>
2023-08-14 13:14:02 +0900

    from local

--- scripts/whale/ft_data2vec_adp.sh
+++ scripts/whale/ft_data2vec_adp.sh
@@ -1,4 +1,4 @@
-for i in {7..9}; do
+for i in {4..6}; do
 	fairseq-hydra-train \
 		--config-dir /home/work/workspace/fairseq/examples/wav2vec/config/finetuning \
 		--config-name base_100h \
@@ -17,7 +17,7 @@ for i in {7..9}; do
 		+task.min_sample_size=16000 \
 		+model.layer_type=trf_adp \
 		+model.adp_num=1 \
-		+model.adp_trf_idx=0:$i
+		+model.adp_trf_idx=$i:12
 done
 
 : <<'END'
