86ffb322f414801f112be60ccae6cba5dc1ad34c
dohe0342 <kimdohe1070@gmail.com>
2023-08-14 14:43:28 +0900

    from local

--- scripts/whale/ft_data2vec_adp.sh
+++ scripts/whale/ft_data2vec_adp.sh
@@ -1,3 +1,24 @@
+fairseq-hydra-train \
+	--config-dir /home/work/workspace/fairseq/examples/wav2vec/config/finetuning \
+	--config-name base_100h \
+	common.user_dir=examples/data2vec \
+	common.tensorboard_logdir=/home/work/workspace/fairseq/scripts/whale/outputs/$1_first_$i \
+	common.log_file=/home/work/workspace/fairseq/scripts/whale/outputs/$1_first_$i.log \
+	task.data=/home/work/workspace/LibriSpeech/manifests \
+	task.normalize=true \
+	model.w2v_path=/home/work/workspace/models/data2vec_model/audio_base_ls.pt \
+	checkpoint.save_dir=/home/work/workspace/fairseq/scripts/whale/outputs/$1_first_$i \
+	dataset.train_subset=ted2_train \
+	dataset.valid_subset=ted2_dev \
+	optimization.max_update=0 \
+	optimization.max_epoch=30 \
+	optimization.lr=[0.0001] \
+	criterion._name=ctc \
+	+task.min_sample_size=16000 \
+	+model.layer_type=trf_adp \
+	+model.adp_num=1 \
+	+model.adp_trf_idx=$i:12
+
 for i in {1..11}; do
 	fairseq-hydra-train \
 		--config-dir /home/work/workspace/fairseq/examples/wav2vec/config/finetuning \
