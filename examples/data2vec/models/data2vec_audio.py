3c85ddf74c1971fc8a484fdeca8328cf517cdee8
dohe0342 <kimdohe1070@gmail.com>
2023-08-16 14:13:43 +0900

    from local

--- examples/data2vec/models/data2vec_audio.py
+++ examples/data2vec/models/data2vec_audio.py
@@ -350,6 +350,7 @@ class Data2VecAudioModel(BaseFairseqModel):
             prompt = prompt.expand((features.size()[0], prompt.size()[0], prompt.size()[1]))
             # scale gradients (this only affects backward, not forward)
             prompt = GradMultiply.apply(prompt, 100)
+
             features = torch.cat([prompt, features], dim=1)
             prompt_padding_mask = torch.zeros(prompt.size()[0], prompt.size()[1]).type(torch.BoolTensor).to(features.device)
             try: padding_mask = torch.cat([prompt_padding_mask, padding_mask], dim=1)
