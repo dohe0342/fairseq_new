aba0f6c395ff76d6e0e7a82d9a4c85acc2419e2c
dohe0342 <kimdohe1070@gmail.com>
2023-08-17 15:29:27 +0900

    from local

--- fairseq/trainer.py
+++ fairseq/trainer.py
@@ -292,7 +292,7 @@ class Trainer(object):
                 logger.info(f"{n} requires grad = True")
                 p.requires_grad = True
             else:
-                p.requries_grad = False
+                p.requires_grad = False
             
         if (
             self.cfg.optimization.debug_param_names
@@ -308,13 +308,6 @@ class Trainer(object):
                     params.append(p)
                     self.param_names.append(n)
         else:
-            for n, p in self.model.named_parameters():
-                if 'adapter' in n or 'norm' in n:
-                    logger.info(f"{n} requires grad = True")
-                    p.requires_grad = True
-                else:
-                    p.requries_grad = False
-
             params = list(
                 filter(
                     lambda p: p.requires_grad,
