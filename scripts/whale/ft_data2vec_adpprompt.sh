f6d8225a6d0d019b81afd062e2dd0a18c34c2da1
dohe0342 <kimdohe1070@gmail.com>
2023-09-01 16:10:57 +0900

    from local

--- scripts/whale/ft_data2vec_adpprompt.sh
+++ scripts/whale/ft_data2vec_adpprompt.sh
@@ -35,7 +35,7 @@ fairseq-hydra-train \
 	optimization.max_update=80000 \
 	optimization.max_epoch=100 \
 	optimization.lr=[0.0002] \
-	criterion._name=ctc \
+	criterion._name=prompt \
 	+task.min_sample_size=16000 \
 	+model.layer_type=trf_adp \
 	+model.adp_num=1
