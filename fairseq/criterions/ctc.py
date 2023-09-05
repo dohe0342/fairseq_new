fe8b8bc07c571ac2ef8ff50b07b9256277139815
dohe0342 <kimdohe1070@gmail.com>
2023-09-05 20:38:25 +0900

    from local

--- fairseq/criterions/ctc.py
+++ fairseq/criterions/ctc.py
@@ -839,7 +839,6 @@ class PromptCtcCriterion(CtcCriterion):
         self, cfg: CtcCriterionConfig, task: FairseqTask, rdrop_alpha: int = 0.0
     ):
         super().__init__(cfg, task, rdrop_alpha)
-        '''
         statistic = open(f'/home/work/workspace/icefall/egs/librispeech/ASR/conv_feat/1284/1284_statistic.txt', 'r').readlines()
         new_emb = torch.empty(512, 50)
         for i in range(512):
@@ -850,8 +849,7 @@ class PromptCtcCriterion(CtcCriterion):
         new_emb = new_emb.transpose(1,0)
         self.prompt = torch.nn.Parameter(new_emb)
         print(self.prompt)
-        '''
-        self.prompt = torch.nn.Parameter(torch.randn(50, 512)/10.)
+        #self.prompt = torch.nn.Parameter(torch.randn(50, 512)/10.)
         #self.prompt = torch.nn.Parameter(torch.rand(50, 512))
         
     def forward(self, model, sample, reduce=True, **kwargs):
