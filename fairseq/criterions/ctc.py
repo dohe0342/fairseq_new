8c523e3e1ea6a3bf60a38f3455877f3c84d91fd8
dohe0342 <kimdohe1070@gmail.com>
2023-09-05 20:40:48 +0900

    from local

--- fairseq/criterions/ctc.py
+++ fairseq/criterions/ctc.py
@@ -848,7 +848,8 @@ class PromptCtcCriterion(CtcCriterion):
             new_emb[i] = torch.normal(float(mean), float(std), size=(1,50)).squeeze()
         new_emb = new_emb.transpose(1,0)
         self.prompt = torch.nn.Parameter(new_emb)
-        print(self.prompt)
+        self.prompt = self.prompt.half()
+        #print(self.prompt)
         #self.prompt = torch.nn.Parameter(torch.randn(50, 512)/10.)
         #self.prompt = torch.nn.Parameter(torch.rand(50, 512))
         
