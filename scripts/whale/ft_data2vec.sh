160481d5c105ebbcdf7cddf8bc6fd1cf4a75eb3a
dohe0342 <kimdohe1070@gmail.com>
2023-09-10 02:28:06 +0900

    from local

--- scripts/whale/ft_data2vec.sh
+++ scripts/whale/ft_data2vec.sh
@@ -12,7 +12,7 @@ fairseq-hydra-train \
 	dataset.valid_subset=ted2_dev \
 	optimization.max_update=80000 \
 	optimization.lr=[0.0003] \
-	criterion._name=prefix \
+	criterion._name=ctc \
 	+task.min_sample_size=16000 \
 
 : <<'END'
