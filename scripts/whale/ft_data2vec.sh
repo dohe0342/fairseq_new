219c7dd17cfca5f31455f39af6fa7c8ee69624b5
dohe0342 <kimdohe1070@gmail.com>
2023-09-12 16:52:28 +0900

    from local

--- scripts/whale/ft_data2vec.sh
+++ scripts/whale/ft_data2vec.sh
@@ -1,18 +1,18 @@
 fairseq-hydra-train \
 	--config-dir /home/work/workspace/fairseq/examples/wav2vec/config/finetuning \
-	--config-name vox_100h \
+	--config-name base_100h \
 	common.user_dir=examples/data2vec \
 	common.tensorboard_logdir=/home/work/workspace/fairseq/scripts/whale/outputs/$1 \
 	common.log_file=/home/work/workspace/fairseq/scripts/whale/outputs/$1.log \
 	task.data=/home/work/workspace/LibriSpeech/manifests \
 	task.normalize=true \
-	model.w2v_path=/home/work/workspace/models/data2vec_model/vox_pretrained.pt \
+	model.w2v_path=/home/work/workspace/models/data2vec_model/audio_base_ls.pt \
 	checkpoint.save_dir=/home/work/workspace/fairseq/scripts/whale/outputs/$1 \
 	dataset.train_subset=ted2_train \
 	dataset.valid_subset=ted2_dev \
 	optimization.max_update=80000 \
 	optimization.lr=[0.0001] \
-	criterion._name=prompt \
+	criterion._name=ctc \
 	+task.min_sample_size=16000 \
 
 : <<'END'
