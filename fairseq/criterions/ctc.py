71197ff660de348caf0bf4146c1fd5f87a82aebf
dohe0342 <kimdohe1070@gmail.com>
2023-09-18 15:31:51 +0900

    from local

--- fairseq/criterions/ctc.py
+++ fairseq/criterions/ctc.py
@@ -1449,7 +1449,7 @@ class CtcCriterion(FairseqCriterion):
 
 def Linear(in_features, out_features, bias=True):
     m = torch.nn.Linear(in_features, out_features, bias)
-    nn.init.xavier_uniform_(m.weight)
+    torch.nn.init.xavier_uniform_(m.weight)
     if bias:
-        nn.init.constant_(m.bias, 0.0)
+        torch.nn.init.constant_(m.bias, 0.0)
     return m
