778ee8742e199b9b11629887c950e65ebd5feca1
김도희 <dohee@macmini.local>
2023-09-19 00:13:36 +0900

    from local

--- fairseq/criterions/ctc.py
+++ fairseq/criterions/ctc.py
@@ -1243,6 +1243,8 @@ class CtcCriterion(FairseqCriterion):
             
             lm_am_sim = torch.bmm(am_output, lm_output.transpose(1, 2))
             lm_am_sim = F.log_softmax(lm_am_sim, dim=-1)
+            if np.random.rand() < 0.1:
+                print(lm_am_sim[0])
             lm_am_sim = F.pad(lm_am_sim, (1, 0, 0, 0, 0, 0), value=np.log(np.e**-1))
             lm_am_sim = lm_am_sim.transpose(0, 1).contiguous()
 
