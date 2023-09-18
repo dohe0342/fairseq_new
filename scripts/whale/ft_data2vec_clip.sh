b8bf9be70022546cc8aebd4f458b94ffdde3b7d9
dohe0342 <kimdohe1070@gmail.com>
2023-09-18 17:20:41 +0900

    from local

--- scripts/whale/ft_data2vec_clip.sh
+++ scripts/whale/ft_data2vec_clip.sh
@@ -6,6 +6,7 @@ fairseq-hydra-train \
 	common.log_file=/home/work/workspace/fairseq/scripts/whale/outputs/$1.log \
 	task.data=/home/work/workspace/LibriSpeech/manifests \
 	task.normalize=true \
+	model._name=wav2vec_ctc_distill \
 	model.w2v_path=/home/work/workspace/models/data2vec_model/audio_base_ls.pt \
 	checkpoint.save_dir=/home/work/workspace/fairseq/scripts/whale/outputs/$1 \
 	dataset.train_subset=train-100 \
