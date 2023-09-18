0520d5fd475b3f6a4e8b1a4146ab4c49a9cdf832
dohe0342 <kimdohe1070@gmail.com>
2023-09-18 16:59:02 +0900

    from local

--- fairseq/criterions/ctc.py
+++ fairseq/criterions/ctc.py
@@ -1311,6 +1311,12 @@ class CtcCriterion(FairseqCriterion):
             print('2', targets_flat.dtype)
             print('3', input_lengths.dtype)
             print('4', target_lengths,dtype)
+                
+            print('5', lm_am_sim.dtype)
+            print('6', alignment_flat.dtype)
+            print('7', input_lengths.dtype)
+            print('8', alignment_lengths.dtype)
+            
             distill_loss = F.ctc_loss(
                 lm_am_sim,
                 alignment_flat,
