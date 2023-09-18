a2a377f817325209ebb7c49ecfd20aea3d6798b4
dohe0342 <kimdohe1070@gmail.com>
2023-09-18 14:00:43 +0900

    from local

--- fairseq_cli/train.py
+++ fairseq_cli/train.py
@@ -100,6 +100,7 @@ def main(cfg: FairseqConfig) -> None:
             model = fsdp_wrap(task.build_model(cfg.model))
     else:
         model = task.build_model(cfg.model)
+        if 0:
         for n, p in model.named_parameters():
             try: 
                 p.data = ckp['model'][n]
