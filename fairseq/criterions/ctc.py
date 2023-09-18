93f3a040643065f57b79f13178b831f33af1951a
dohe0342 <kimdohe1070@gmail.com>
2023-09-18 16:23:46 +0900

    from local

--- fairseq/criterions/ctc.py
+++ fairseq/criterions/ctc.py
@@ -1240,6 +1240,7 @@ class CtcCriterion(FairseqCriterion):
         
         lm_am_sim = torch.bmm(lm_output, am_output.transpose(1, 2))
         lm_am_sim = torch.nn.functional.log_softmax(lm_am_sim, dim=-1)
+        lm_am_sim = lm_am_sim.transpose(0, 1).contiguous()
 
         print(lm_am_sim.size())
         exit()
