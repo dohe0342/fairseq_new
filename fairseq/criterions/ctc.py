145fa42b6b380f318f91c88ae3e89a1ca4f0ddaf
김도희 <dohee@macmini.local>
2023-09-23 20:57:44 +0900

    from local

--- fairseq/criterions/ctc.py
+++ fairseq/criterions/ctc.py
@@ -1236,12 +1236,12 @@ class CtcCriterion(FairseqCriterion):
             with torch.no_grad():
                 lm_output = self.lm(**lm_input)
                 lm_output = lm_output['last_hidden_state']
-                #lm_output = F.normalize(lm_output, dim=2)
+                lm_output = F.normalize(lm_output, dim=2)
 
             am_output = net_output['encoder_feat'].transpose(0, 1) ## T x B x C -> B x T x C
-            am_output = F.gelu(am_output)
+            #am_output = F.gelu(am_output)
             am_output = self.lm_linear(am_output)
-            #am_output = F.normalize(am_output, dim=2)
+            am_output = F.normalize(am_output, dim=2)
             
             lm_am_sim = torch.bmm(am_output, lm_output.transpose(1, 2))
             if np.random.rand() < 0.1 and 0:
