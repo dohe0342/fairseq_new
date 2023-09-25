b845f58adb2192c28517e4ac7bd546a626ee8f5d
dohe0342 <kimdohe1070@gmail.com>
2023-09-25 16:40:24 +0900

    from local

--- fairseq/trainer.py
+++ fairseq/trainer.py
@@ -317,9 +317,9 @@ class Trainer(object):
                     chain(self.model.parameters(), self.criterion.parameters()),
                 )
             )
-            for n, p in self.model.named_parameters():
-                if p.requires_grad:
-                    logger.info(n)
+            #for n, p in self.model.named_parameters():
+            #    if p.requires_grad:
+            #        logger.info(n)
         
         logger.info("-----------------------------------------")
         logger.info(len(params))
