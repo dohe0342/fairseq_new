410934e4184833339c85c99f5dd16a21263453ad
김도희 <dohee@macmini.local>
2023-09-28 15:12:28 +0900

    from local

--- fairseq/criterions/ctc.py
+++ fairseq/criterions/ctc.py
@@ -1247,8 +1247,8 @@ class CtcCriterion(FairseqCriterion):
             am_output = net_output['encoder_feat'].transpose(0, 1) ## T x B x C -> B x T x C
             #am_output = F.gelu(am_output)
             am_output = self.lm_linear(am_output)
-            am_output = self.ins_norm(am_output)
-            #am_output = F.normalize(am_output, dim=2)
+            #am_output = self.ins_norm(am_output)
+            am_output = F.normalize(am_output, dim=2)
             
             lm_am_sim = torch.bmm(am_output, lm_output.transpose(1, 2))
             if np.random.rand() < 0.1 and 0:
