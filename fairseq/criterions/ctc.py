2339e1eb3a34fa6d0a8968c226a115c05ebe2e19
dohe0342 <kimdohe1070@gmail.com>
2023-10-05 12:45:10 +0900

    from local

--- fairseq/criterions/ctc.py
+++ fairseq/criterions/ctc.py
@@ -843,14 +843,15 @@ class PromptCtcCriterion(CtcCriterion):
         self, cfg: CtcCriterionConfig, task: FairseqTask, rdrop_alpha: int = 0.0
     ):
         super().__init__(cfg, task, rdrop_alpha)
-        statistic = open(f'/home/work/workspace/icefall/egs/librispeech/ASR/conv_feat/1284/1284_statistic.txt', 'r').readlines()
-        new_emb = torch.empty(512, 50)
-        for i in range(512):
-            mean, std = statistic[i].strip().split(' ')
-            #print(new_emb[i].size())
-            #print(float(mean), float(std))
-            new_emb[i] = torch.normal(float(mean), float(std), size=(1,50)).squeeze()
-        new_emb = new_emb.transpose(1,0)
+        if 0:
+            statistic = open(f'/home/work/workspace/icefall/egs/librispeech/ASR/conv_feat/1284/1284_statistic.txt', 'r').readlines()
+            new_emb = torch.empty(512, 50)
+            for i in range(512):
+                mean, std = statistic[i].strip().split(' ')
+                #print(new_emb[i].size())
+                #print(float(mean), float(std))
+                new_emb[i] = torch.normal(float(mean), float(std), size=(1,50)).squeeze()
+            new_emb = new_emb.transpose(1,0)
         #self.prompt = torch.nn.Parameter(new_emb).half()
         #self.prompt = torch.nn.Parameter(new_emb)
         self.prompt = torch.nn.Parameter(torch.randn(50, 512)/10.)
