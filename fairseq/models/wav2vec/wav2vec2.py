c02dbc6e6ea2d9209170683f239b1029b0abdaea
김도희 <dohee@macmini.local>
2023-10-11 00:53:05 +0900

    from local

--- fairseq/models/wav2vec/wav2vec2.py
+++ fairseq/models/wav2vec/wav2vec2.py
@@ -654,6 +654,8 @@ class Wav2Vec2Model(BaseFairseqModel):
                     else:
                         try: noise_prompt = noise_prompt.cat([noise_prompt, prompt[1].unsqueeze(0)], dim=0)
                         except: noise_prompt = prompt[1].unsqueeze(0)
+
+                prompt = nosie_prompt
             else:
                 prompt = prompt.expand((features.size()[0], prompt.size()[0], prompt.size()[1]))
             
