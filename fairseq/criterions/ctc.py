80af056b417da10f32f0f4087e447bead714d213
dohe0342 <kimdohe1070@gmail.com>
2023-10-10 13:08:32 +0900

    from local

--- fairseq/criterions/ctc.py
+++ fairseq/criterions/ctc.py
@@ -1243,7 +1243,7 @@ class CtcCriterion(FairseqCriterion):
             with torch.no_grad():
                 lm_output = self.lm(**lm_input)
                 lm_output = lm_output['last_hidden_state']
-                if 1:
+                if 10:
                     lm_output = lm_output[:,1:,:]
                 lm_output = F.normalize(lm_output, dim=2)
 
