91027c533c6e8442bdb4f9d584be59bb40c3b8d7
dohe0342 <kimdohe1070@gmail.com>
2023-10-10 16:43:05 +0900

    from local

--- scripts/whale/ft_data2vec_adpprompt.sh
+++ scripts/whale/ft_data2vec_adpprompt.sh
@@ -4,7 +4,7 @@ fairseq-hydra-train \
 	--config-name base_100h \
 	common.user_dir=examples/data2vec \
 	common.tensorboard_logdir=/home/work/workspace/fairseq/scripts/whale/outputs/adapter_prompt_dim$dim \
-	common.log_file=/home/work/workspace/fairseq/scripts/whale/outputs/dim"$dim".log \
+	common.log_file=/home/work/workspace/fairseq/scripts/whale/outputs/adapter_prompt_dim$dim.log \
 	task.data=/home/work/workspace/LibriSpeech/manifests \
 	task.normalize=true \
 	model.w2v_path=/home/work/workspace/models/data2vec_model/audio_base_ls.pt \
