808fd25408b681964632eefa3d877639605f916e
dohe0342 <kimdohe1070@gmail.com>
2023-10-10 13:49:26 +0900

    from local

--- fairseq/criterions/ctc.py
+++ fairseq/criterions/ctc.py
@@ -1312,7 +1312,7 @@ class CtcCriterion(FairseqCriterion):
         #############for alignment target ###############################
         #alignment_pad_mask = lm_input["attention_mask"] > 0
         alignment_lengths = torch.sum(lm_input["attention_mask"], 1)
-        if 1:
+        if 0:
             alignment_lengths -= 1
         alignment_flat = torch.linspace(
                                             1, 
