e8401ae91504e23ad8d68a46745ea6f50bca8b45
dohe0342 <kimdohe1070@gmail.com>
2023-10-12 12:51:20 +0900

    from local

--- scripts/whale/ft_data2vec.sh
+++ scripts/whale/ft_data2vec.sh
@@ -4,14 +4,14 @@ fairseq-hydra-train \
 	common.user_dir=examples/data2vec \
 	common.tensorboard_logdir=/home/work/workspace/fairseq/scripts/whale/outputs/$1 \
 	common.log_file=/home/work/workspace/fairseq/scripts/whale/outputs/$1.log \
-	task.data=/home/work/workspace/LibriSpeech/manifests \
+	task.data=/dev/shm/manifests \
 	model.w2v_path=/home/work/workspace/models/wav2vec_model/wav2vec_small.pt \
 	checkpoint.save_dir=/home/work/workspace/fairseq/scripts/whale/outputs/$1 \
 	dataset.train_subset=train-100-noisy \
 	dataset.valid_subset=dev-other \
 	criterion._name=ctc
 
-#task.data=/dev/shm/manifests \
+#task.data=/home/work/workspace/LibriSpeech/manifests \
 : <<'END'
 fairseq-hydra-train \
 	--config-dir /home/work/workspace/fairseq/examples/wav2vec/config/finetuning \
