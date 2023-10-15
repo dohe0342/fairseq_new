9e499c815e9ce38dc5aaa5c6f44f31cb62dfecd9
김도희 <dohee@macmini.local>
2023-10-16 02:02:18 +0900

    from local

--- scripts/whale/ft_data2vec.sh
+++ scripts/whale/ft_data2vec.sh
@@ -9,7 +9,7 @@ fairseq-hydra-train \
 	task.data=/dev/shm/manifests \
 	model.w2v_path=/home/work/workspace/models/wav2vec_model/wav2vec_small.pt \
 	checkpoint.save_dir=/home/work/workspace/fairseq/scripts/whale/outputs/$exp_name \
-	dataset.train_subset=train-200-speech \
+	dataset.train_subset=train-200-$noise \
 	dataset.valid_subset=dev-other \
 	criterion._name=prompt2
 
