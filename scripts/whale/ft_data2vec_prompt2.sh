1c7ad3ed99a1e996c0e41a0aada3db9826cc8e27
김도희 <dohee@macmini.local>
2023-10-19 00:41:56 +0900

    from local

--- scripts/whale/ft_data2vec_prompt2.sh
+++ scripts/whale/ft_data2vec_prompt2.sh
@@ -1,5 +1,5 @@
 noise=$1
-exp_name="w2v2_200h_clean+"$noise"_prompt-freeze30000"
+exp_name="w2v2_200h_clean+"$noise"_prompt-freeze20000"
 fairseq-hydra-train \
 	--config-dir /home/work/workspace/fairseq/examples/wav2vec/config/finetuning \
 	--config-name base_100h \
