d4b156bf50f70793af6d388de376d6fb312e8a0c
dohe0342 <kimdohe1070@gmail.com>
2023-10-19 14:02:53 +0900

    from local

--- fairseq/criterions/ctc.py
+++ fairseq/criterions/ctc.py
@@ -1901,14 +1901,7 @@ class Clip2Criterion(FairseqCriterion):
             am_output = F.normalize(am_output, dim=2)
             
             lm_am_sim = torch.bmm(am_output, lm_output.transpose(1, 2))
-            if np.random.rand() < 0.1 and 0:
-                softmax = F.softmax(lm_am_sim / 3, dim=-1)
-                print(softmax.size(), softmax[0][0].size(), softmax[0][0])
-                print(softmax[0][100])
-                print(softmax[0][200])
-                print(softmax[0][300])
-                print('-'*20)
-
+            
             lm_am_sim = F.log_softmax(lm_am_sim, dim=-1)
             lm_am_sim = F.pad(lm_am_sim, (1, 0, 0, 0, 0, 0), value=np.log(np.e**-1))
             lm_am_sim = lm_am_sim.transpose(0, 1).contiguous()
