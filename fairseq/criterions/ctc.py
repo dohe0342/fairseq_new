1bd871bbceae97d08e6c5ba3eb29ffe1ed3efa5a
dohe0342 <kimdohe1070@gmail.com>
2023-10-19 11:26:33 +0900

    from local

--- fairseq/criterions/ctc.py
+++ fairseq/criterions/ctc.py
@@ -22,7 +22,7 @@ from fairseq.dataclass import FairseqDataclass
 from fairseq.data.data_utils import post_process
 from fairseq.tasks import FairseqTask
 from fairseq.logging.meters import safe_round
-from fairseq.models.wav2vec.wav2vec2_asr import Linearm Wav2Vec2Seq2SeqConfig, LanguageModelDistillationDecoder
+from fairseq.models.wav2vec.wav2vec2_asr import Linear, Wav2Vec2Seq2SeqConfig, LanguageModelDistillationDecoder
 
 from transformers import GPT2Tokenizer, GPT2Model, BertTokenizer, BertModel
 from fairseq.data.data_utils import post_process
