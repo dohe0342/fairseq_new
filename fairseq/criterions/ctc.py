653cdb886fa6e60ff25b6afeddcc61fd0e06541b
dohe0342 <kimdohe1070@gmail.com>
2023-10-19 13:10:41 +0900

    from local

--- fairseq/criterions/ctc.py
+++ fairseq/criterions/ctc.py
@@ -1818,6 +1818,7 @@ class Clip2Criterion(FairseqCriterion):
             lm_cfg = Wav2Vec2Seq2SeqConfig()
             self.lm_decoder = LanguageModelDistillationDecoder.build_model(lm_cfg, task)
             temp = torch.rand(1, 10, 768)
+            temp2 = self.lm_decoder(temp)
         
         ##############################################################
         self.blank_idx = (
