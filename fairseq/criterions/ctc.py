422ac55a108f41b5b3bf2287fe1cd5150a252a21
dohe0342 <kimdohe1070@gmail.com>
2023-10-20 11:32:58 +0900

    from local

--- fairseq/criterions/ctc.py
+++ fairseq/criterions/ctc.py
@@ -1895,16 +1895,21 @@ class Clip2Criterion(FairseqCriterion):
                 #lm_output = F.normalize(lm_output, dim=2)
             
             am_output = net_output['encoder_feat'].transpose(0, 1) ## T x B x C -> B x T x C
-            am_output = GradMultiply.apply(am_output, 0.001)
+            #am_output = GradMultiply.apply(am_output, 0.001)
             am_output = self.lm_decoder(am_output)
             if type(am_output) == tuple: am_output = am_output[0]
-            am_output = F.normalize(am_output, dim=2)
-            
-            lm_am_sim = torch.bmm(am_output, lm_output.transpose(1, 2))
-            
-            lm_am_sim = F.log_softmax(lm_am_sim, dim=-1)
-            lm_am_sim = F.pad(lm_am_sim, (1, 0, 0, 0, 0, 0), value=np.log(np.e**-1))
-            lm_am_sim = lm_am_sim.transpose(0, 1).contiguous()
+            if 0:
+                am_output = F.normalize(am_output, dim=2)
+                
+                lm_am_sim = torch.bmm(am_output, lm_output.transpose(1, 2))
+                
+                lm_am_sim = F.log_softmax(lm_am_sim, dim=-1)
+                lm_am_sim = F.pad(lm_am_sim, (1, 0, 0, 0, 0, 0), value=np.log(np.e**-1))
+                lm_am_sim = lm_am_sim.transpose(0, 1).contiguous()
+            if 1:
+                lm_am_dist = am_output.unsqueeze(2) - lm_output.unsqueeze(1)
+                lm_am_dist = torch.norm(lm_am_dist, p=2, dim=3)
+                lm_am_sim = lm_am_dist
 
             lm_am_sim = GradMultiply.apply(lm_am_sim, 1000)
 
