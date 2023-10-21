d1ced6e05564044ba91ec73bcc3284bfc2f8f1fd
dohe0342 <kimdohe1070@gmail.com>
2023-10-21 10:09:44 +0900

    from local

--- fairseq/criterions/ctc.py
+++ fairseq/criterions/ctc.py
@@ -1896,7 +1896,7 @@ class Clip2Criterion(FairseqCriterion):
             #am_output = GradMultiply.apply(am_output, 0.001)
             am_output = self.lm_decoder(am_output)
             if type(am_output) == tuple: am_output = am_output[0]
-            am_output = GradMultiply.apply(am_output, 1000)
+            #am_output = GradMultiply.apply(am_output, 1000)
             if 0:
                 lm_output = F.normalize(lm_output, dim=2)
                 am_output = F.normalize(am_output, dim=2)
