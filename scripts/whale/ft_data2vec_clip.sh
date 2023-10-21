a63b175aa0c50b1cfc434d971a5320557a7e4e3a
dohe0342 <kimdohe1070@gmail.com>
2023-10-21 12:12:56 +0900

    from local

--- scripts/whale/ft_data2vec_clip.sh
+++ scripts/whale/ft_data2vec_clip.sh
@@ -11,7 +11,7 @@ fairseq-hydra-train \
 	checkpoint.save_dir=/home/work/workspace/fairseq/scripts/whale/outputs/$1 \
 	dataset.max_tokens=3200000 \
 	optimization.update_freq=[1] \
-	optimization.lr=[0.3] \
+	optimization.lr=[0.1] \
 	criterion._name=clip2
 
 : <<'END'
