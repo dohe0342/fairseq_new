a8217f710f24931bd74a94c6635f50c4cdf38c58
김도희 <dohee@macmini.local>
2023-10-23 21:22:39 +0900

    from local

--- fairseq/criterions/ctc.py
+++ fairseq/criterions/ctc.py
@@ -1905,6 +1905,7 @@ class Clip2Criterion(FairseqCriterion):
             
             am_output = self.lm_linear2(am_output)
             am_output = self.ln(am_output)
+            am_output = GradMultiply(am_output, 1000)
             
             if 0:
                 lm_output = F.normalize(lm_output, dim=2)
