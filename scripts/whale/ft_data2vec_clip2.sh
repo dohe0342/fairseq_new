384952c0e4bad1dd62d8c99ca6b1b4b34a3888d2
dohe0342 <kimdohe1070@gmail.com>
2023-10-23 16:16:22 +0900

    from local

--- scripts/whale/ft_data2vec_clip2.sh
+++ scripts/whale/ft_data2vec_clip2.sh
@@ -2,12 +2,14 @@ fairseq-hydra-train \
 	--config-dir /workspace/fairseq_new/examples/wav2vec/config/finetuning \
 	--config-name base_100h \
 	common.log_file=/workspace/fairseq_new/scripts/whale/outputs/$1.log \
+	common.fp16_no_flatten_grads=true \
 	task.data=/DB/manifests \
 	model.w2v_path=/workspace/models/wav2vec_small.pt \
 	model.freeze_finetune_updates=10000 \
 	checkpoint.save_dir=/workspace/fairseq_new/scripts/whale/outputs/$1 \
 	dataset.max_tokens=3200000 \
 	optimization.update_freq=[2] \
+	optimization.debug_param_names=true \
 	criterion._name=clip2
 
 #common.wandb_project=lm2am_distill \
