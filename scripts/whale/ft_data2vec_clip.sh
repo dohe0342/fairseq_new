afb042a86e6da584fd6cc56921e1dba1562ae422
김도희 <dohee@macmini.local>
2023-10-23 21:48:54 +0900

    from local

--- scripts/whale/ft_data2vec_clip.sh
+++ scripts/whale/ft_data2vec_clip.sh
@@ -10,6 +10,7 @@ fairseq-hydra-train \
 	checkpoint.save_dir=/home/work/workspace/fairseq/scripts/whale/outputs/$1 \
 	dataset.max_tokens=3200000 \
 	optimization.update_freq=[1] \
+	optimization.lr=[0.01] \
 	criterion._name=clip2
 
 : <<'END'
