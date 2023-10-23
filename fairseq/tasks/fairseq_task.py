a69f87a41e88c3e75b2d269decc18c8743eda0e1
dohe0342 <kimdohe1070@gmail.com>
2023-10-23 14:30:05 +0900

    from local

--- fairseq/tasks/fairseq_task.py
+++ fairseq/tasks/fairseq_task.py
@@ -533,7 +533,7 @@ class FairseqTask(object):
         model.set_num_updates(update_num)
         with torch.autograd.profiler.record_function("forward"):
             with torch.cuda.amp.autocast(enabled=(isinstance(optimizer, AMPOptimizer))):
-                loss, sample_size, logging_output = criterion(model, sample)
+                (loss, distill_loss), sample_size, logging_output = criterion(model, sample)
         if ignore_grad:
             loss *= 0
         with torch.autograd.profiler.record_function("backward"):
