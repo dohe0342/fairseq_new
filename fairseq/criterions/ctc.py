920a096e8ec616b397fa03ebbbcf459768cabc14
dohe0342 <kimdohe1070@gmail.com>
2023-10-23 13:05:52 +0900

    from local

--- fairseq/criterions/ctc.py
+++ fairseq/criterions/ctc.py
@@ -1915,12 +1915,10 @@ class Clip2Criterion(FairseqCriterion):
                 lm_am_dist = torch.norm(lm_am_dist, p=2, dim=3)
                 lm_am_sim = -lm_am_dist
 
-            am_output = GradMultiply.apply(am_output, 3000)
-            
             lm_am_sim = F.log_softmax(lm_am_sim, dim=-1)
             lm_am_sim = F.pad(lm_am_sim, (1, 0, 0, 0, 0, 0), value=np.log(np.e**-1))
             lm_am_sim = lm_am_sim.transpose(0, 1).contiguous()
-            #lm_am_sim = GradMultiply.apply(lm_am_sim, 1000)
+            lm_am_sim = GradMultiply.apply(lm_am_sim, 3000)
 
         ##############################
 
