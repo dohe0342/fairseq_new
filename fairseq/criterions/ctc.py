660e279311032152b4584f2e2f989cf4c82423ab
dohe0342 <kimdohe1070@gmail.com>
2023-10-23 13:28:48 +0900

    from local

--- fairseq/criterions/ctc.py
+++ fairseq/criterions/ctc.py
@@ -1918,7 +1918,7 @@ class Clip2Criterion(FairseqCriterion):
             lm_am_sim = F.log_softmax(lm_am_sim, dim=-1)
             lm_am_sim = F.pad(lm_am_sim, (1, 0, 0, 0, 0, 0), value=np.log(np.e**-1))
             lm_am_sim = lm_am_sim.transpose(0, 1).contiguous()
-            lm_am_sim = GradMultiply.apply(lm_am_sim, 100000)
+            lm_am_sim = GradMultiply.apply(lm_am_sim, 10000)
 
         ##############################
 
