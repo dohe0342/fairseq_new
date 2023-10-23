25fce0bb558e073bd714e1d2ebdf4ee8ee656a60
김도희 <dohee@macmini.local>
2023-10-23 21:25:20 +0900

    from local

--- fairseq/criterions/ctc.py
+++ fairseq/criterions/ctc.py
@@ -1899,7 +1899,7 @@ class Clip2Criterion(FairseqCriterion):
                 lm_output = lm_output['last_hidden_state']
             
             am_output = net_output['encoder_feat'].transpose(0, 1) ## T x B x C -> B x T x C
-            am_output = GradMultiply(am_output, 0.001)
+            am_output = GradMultiply.apply(am_output, 0.001)
             am_output = self.lm_decoder(am_output)
             if type(am_output) == tuple: am_output = am_output[0]
             
