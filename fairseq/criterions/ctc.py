2fed58d9dd887143aeec6160b2ba63df3b37be7c
dohe0342 <kimdohe1070@gmail.com>
2023-10-24 10:34:07 +0900

    from local

--- fairseq/criterions/ctc.py
+++ fairseq/criterions/ctc.py
@@ -1924,7 +1924,7 @@ class Clip2Criterion(FairseqCriterion):
 
             lm_am_sim = F.log_softmax(lm_am_sim, dim=-1)
             for b in lm_am_sim.size(0):
-                plt.imshow(lm_am_sim[0])
+                plt.imshow(lm_am_sim[b])
                 plt.savefig('alingment')
             lm_am_sim = F.pad(lm_am_sim, (1, 0, 0, 0, 0, 0), value=np.log(np.e**-1))
             lm_am_sim = lm_am_sim.transpose(0, 1).contiguous()
