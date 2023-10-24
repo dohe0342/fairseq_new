d0333e3e9057edb84e56cc7c8d7cbb7088a664eb
dohe0342 <kimdohe1070@gmail.com>
2023-10-24 11:59:14 +0900

    from local

--- fairseq/criterions/ctc.py
+++ fairseq/criterions/ctc.py
@@ -1922,8 +1922,8 @@ class Clip2Criterion(FairseqCriterion):
                 lm_am_dist = torch.norm(lm_am_dist, p=2, dim=3)
                 lm_am_sim = -lm_am_dist
 
-            #lm_am_sim = F.log_softmax(lm_am_sim, dim=-1)
-            lm_am_sim = F.softmax(lm_am_sim, dim=-1)
+            lm_am_sim = F.log_softmax(lm_am_sim, dim=-1)
+            #lm_am_sim = F.softmax(lm_am_sim, dim=-1)
             if 0:
                 for b in range(lm_am_sim.size(0)):
                     plt.imshow(lm_am_sim[b].T.detach().cpu().numpy())
