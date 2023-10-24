5d76217fbb3302e2a7dc94667e41204fbe7fe293
김도희 <dohee@macmini.local>
2023-10-24 20:55:21 +0900

    from local

--- fairseq/models/wav2vec/wav2vec2_asr.py
+++ fairseq/models/wav2vec/wav2vec2_asr.py
@@ -985,7 +985,7 @@ class TransformerDecoderForDistill(FairseqIncrementalDecoder):
 
         # TODO: update this when transformer gets converted to dataclass configs
         transformer_cfg = copy.deepcopy(cfg)
-        #with open_dict(transformer_cfg):
+        
         transformer_cfg.dropout = transformer_cfg.decoder_dropout
         transformer_cfg.attention_dropout = (
             transformer_cfg.decoder_attention_dropout
