ee710d169c0cf819bfbd90da0b3d75f53957c6c8
dohe0342 <kimdohe1070@gmail.com>
2023-10-24 12:01:19 +0900

    from local

--- fairseq/criterions/ctc.py
+++ fairseq/criterions/ctc.py
@@ -1930,7 +1930,6 @@ class Clip2Criterion(FairseqCriterion):
                     plt.imshow(lm_am_sim_cp[b].T.cpu().numpy())
                     plt.savefig(f'/home/work/workspace/fairseq/scripts/whale/png/alingment{b}.png')
                     plt.close()
-                exit()
             
             lm_am_sim = F.pad(lm_am_sim, (1, 0, 0, 0, 0, 0), value=np.log(np.e**-1))
             lm_am_sim = lm_am_sim.transpose(0, 1).contiguous()
