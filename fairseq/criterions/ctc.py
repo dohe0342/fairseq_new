a4887df290be504351dbd135cd81eb5f110d56a0
dohe0342 <kimdohe1070@gmail.com>
2023-10-24 15:53:03 +0900

    from local

--- fairseq/criterions/ctc.py
+++ fairseq/criterions/ctc.py
@@ -1520,7 +1520,6 @@ class ClipCriterion(FairseqCriterion):
                 lm_output = lm_output['last_hidden_state']
                 if 0:
                     lm_output = lm_output[:,1:,:]
-                
                 #lm_output = F.normalize(lm_output, dim=2)
 
             am_output = net_output['encoder_feat'].transpose(0, 1) ## T x B x C -> B x T x C
