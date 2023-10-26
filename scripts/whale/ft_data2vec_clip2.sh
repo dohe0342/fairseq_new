54902f5466974791f3aa2cda99fd1efe2afd5e6f
김도희 <dohee@macmini.local>
2023-10-27 00:39:42 +0900

    from local

--- scripts/whale/ft_data2vec_clip2.sh
+++ scripts/whale/ft_data2vec_clip2.sh
@@ -9,7 +9,8 @@ fairseq-hydra-train \
 	checkpoint.save_dir=/workspace/fairseq_new/scripts/whale/outputs/$1 \
 	dataset.max_tokens=3200000 \
 	optimization.update_freq=[2] \
-	criterion._name=clip3
+	criterion._name=clip3 \
+	+criterion.decoder=conv
 
 #common.wandb_project=lm2am_distill \
 : <<'END'
