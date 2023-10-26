a82e81a41c34703271a4e296023e16afb25cec09
dohe0342 <kimdohe1070@gmail.com>
2023-10-26 17:20:30 +0900

    from local

--- fairseq/models/wav2vec/wav2vec2.py
+++ fairseq/models/wav2vec/wav2vec2.py
@@ -1083,7 +1083,7 @@ class TransformerEncoder(nn.Module):
                 )
 
             self.pos_conv = make_conv_block(
-                768, k, args.conv_pos_groups, num_layers
+                self.embedding_dim, k, args.conv_pos_groups, num_layers
             )
 
         else:
