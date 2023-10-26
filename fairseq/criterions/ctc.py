d3cc4bb36de390ec82f533087f1492661b5c0ba4
김도희 <dohee@macmini.local>
2023-10-26 19:38:41 +0900

    from local

--- fairseq/criterions/ctc.py
+++ fairseq/criterions/ctc.py
@@ -2380,6 +2380,7 @@ class Clip3Criterion(FairseqCriterion):
             am_output = net_output['encoder_feat'].transpose(0, 1) ## T x B x C -> B x T x C
             print('1111', am_output.size())
             if self.decoder_type == 'conv':
+                am_output = am_output.transpose(1, 2)
                 for i, conv in enumerate(self.lm_decoder):
                     am_output = conv(am_output)
                     print(i, am_output.size())
