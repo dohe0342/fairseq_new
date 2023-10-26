ef08c1f55f3d7840f101df8d343842900412091a
김도희 <dohee@macmini.local>
2023-10-26 20:05:39 +0900

    from local

--- fairseq/criterions/ctc.py
+++ fairseq/criterions/ctc.py
@@ -2477,6 +2477,8 @@ class Clip3Criterion(FairseqCriterion):
             lm_lengths = input_lengths.clone()
             for i in range(len(self.lm_decoder)):
                 lm_lengths = ((lm_lengths - 5)/2).to(torch.int)
+        else:
+            lm_lengths = input_lengths
         #############for alignment target ###############################
         #alignment_pad_mask = lm_input["attention_mask"] > 0
         alignment_lengths = torch.sum(lm_input["attention_mask"], 1)
