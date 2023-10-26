09d86ba48f72772f32520fb36567cb9eea90b4b5
김도희 <dohee@macmini.local>
2023-10-26 19:22:51 +0900

    from local

--- fairseq/criterions/ctc.py
+++ fairseq/criterions/ctc.py
@@ -2288,7 +2288,7 @@ class Clip3Criterion(FairseqCriterion):
                         conv_bias=False,
                     )
                 )
-            logger.info(self.conv_layers)
+            logging.info(self.conv_layers)
             exit()
                         
         if cfg.decoder == 'transf_enc':
