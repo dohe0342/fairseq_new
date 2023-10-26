450c0ae04b8daf2c100b65254abcb5bd4888f7ba
김도희 <dohee@macmini.local>
2023-10-26 19:27:27 +0900

    from local

--- fairseq/criterions/ctc.py
+++ fairseq/criterions/ctc.py
@@ -2381,7 +2381,7 @@ class Clip3Criterion(FairseqCriterion):
             am_output = self.lm_decoder(am_output, padding_mask)
             if type(am_output) == tuple: am_output = am_output[0]
             
-            am_output = self.lm_linear2(am_output)
+            #am_output = self.lm_linear2(am_output)
             #am_output = self.ln(am_output)
             
             if 1:
