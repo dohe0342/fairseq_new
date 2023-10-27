f8407c137aa8a2017e588990a3703ad91e3acbbc
dohe0342 <kimdohe1070@gmail.com>
2023-10-27 15:42:42 +0900

    from local

--- scripts/whale/ft_data2vec_clip.sh
+++ scripts/whale/ft_data2vec_clip.sh
@@ -9,8 +9,8 @@ fairseq-hydra-train \
 	task.data=/dev/shm/manifests \
 	model.w2v_path=/home/work/workspace/models/wav2vec_model/wav2vec_vox_new.pt \
 	checkpoint.save_dir=/home/work/workspace/fairseq/scripts/whale/outputs/$1 \
-	criterion._name=ctc 
-#+criterion.decoder=conv 
+	criterion._name=clip3 \
+	+criterion.decoder=conv 
 
 : <<'END'
 fairseq-hydra-train \
