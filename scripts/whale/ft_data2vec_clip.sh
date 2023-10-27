f23589202ac5d77d6f78daf3de6d3fc5c66b4bcb
dohe0342 <kimdohe1070@gmail.com>
2023-10-27 14:42:55 +0900

    from local

--- scripts/whale/ft_data2vec_clip.sh
+++ scripts/whale/ft_data2vec_clip.sh
@@ -5,7 +5,7 @@ fairseq-hydra-train \
 	common.wandb_project=lm2am_distill \
 	distributed_training.distributed_world_size=4 \
 	task.data=/dev/shm/manifests \
-	model.w2v_path=/home/work/workspace/models/wav2vec_model/wav2vec_small.pt \
+	model.w2v_path=/home/work/workspace/models/wav2vec_model/wav2vec_vox_new.pt \
 	model.freeze_finetune_updates=0 \
 	checkpoint.save_dir=/home/work/workspace/fairseq/scripts/whale/outputs/$1 \
 	dataset.max_tokens=3200000 \
