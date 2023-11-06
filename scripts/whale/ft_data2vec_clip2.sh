c751d2132d80b772347cd0f89fd9dc461bf97b9b
dohe0342 <kimdohe1070@gmail.com>
2023-11-06 11:03:19 +0900

    from local

--- scripts/whale/ft_data2vec_clip2.sh
+++ scripts/whale/ft_data2vec_clip2.sh
@@ -10,7 +10,8 @@ fairseq-hydra-train \
 	dataset.max_tokens=3200000 \
 	optimization.update_freq=[2] \
 	criterion._name=clip3 \
-	+criterion.decoder=conv 
+	+criterion.decoder=conv \
+	+criterion.lm_decay=0.1
 
 
 #common.wandb_project=lm2am_distill \
