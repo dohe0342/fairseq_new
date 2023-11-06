8cdfae6dbdc3b9db04b44aacd1962c2fab7e601f
dohe0342 <kimdohe1070@gmail.com>
2023-11-06 16:37:30 +0900

    from local

--- fairseq/criterions/ctc.py
+++ fairseq/criterions/ctc.py
@@ -1102,6 +1102,69 @@ class Prompt2CtcCriterion(CtcCriterion):
         ckpt = torch.load('/home/work/workspace/fairseq/scripts/whale/outputs/w2v2_200h_clean+speech_mixed-valid_prompt_prompt-freeze80000_orthogonal/checkpoint_best.pt')
         self.prompt = ckpt['criterion']['prompt']
         self.prompt = torch.nn.Parameter(self.prompt)
+        if self.decoder_type == 'conv':
+            conv_layers = [(d, 5, 2)] * 3
+            mode = "layer_norm"
+            dropout = 0.0
+
+            def block(
+                n_in,
+                n_out,
+                k,
+                stride,
+                groups=1,
+                is_layer_norm=False,
+                is_group_norm=False,
+                conv_bias=False,
+            ):
+                def make_conv():
+                    conv = nn.Conv1d(n_in, n_out, k, stride=stride, bias=conv_bias, groups=groups)
+                    nn.init.kaiming_normal_(conv.weight)
+                    return conv
+
+                assert (
+                    is_layer_norm and is_group_norm
+                ) == False, "layer norm and group norm are exclusive"
+
+                if is_layer_norm:
+                    return nn.Sequential(
+                        make_conv(),
+                        nn.Dropout(p=dropout),
+                        nn.Sequential(
+                            TransposeLast(),
+                            Fp32LayerNorm(dim, elementwise_affine=True),
+                            TransposeLast(),
+                        ),
+                        nn.GELU(),
+                    )
+                elif is_group_norm:
+                    return nn.Sequential(
+                        make_conv(),
+                        nn.Dropout(p=dropout),
+                        Fp32GroupNorm(dim, dim, affine=True),
+                        nn.GELU(),
+                    )
+                else:
+                    return nn.Sequential(make_conv(), nn.Dropout(p=dropout), nn.GELU())
+            
+            self.lm_decoder = nn.ModuleList()
+            for i, cl in enumerate(conv_layers):
+                assert len(cl) == 3, "invalid conv definition: " + str(cl)
+                (dim, k, stride) = cl
+
+                self.lm_decoder.append(
+                    block(
+                        dim,
+                        dim,
+                        k,
+                        stride,
+                        is_layer_norm=(mode == "layer_norm"),
+                        is_group_norm=(mode == "default") and i == 0,
+                        conv_bias=False,
+                    )
+                )
+            if d != self.lm.embed_dim:
+                self.lm_decoder.append(Linear(d, self.lm.embed_dim, bias=False))
         '''
                 
     def hook_fn(self, module, input, output):
