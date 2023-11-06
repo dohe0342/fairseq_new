fb445487618104be4fac7115b27d46846840ac7c
dohe0342 <kimdohe1070@gmail.com>
2023-11-06 13:05:10 +0900

    from local

--- scripts/whale/ft_data2vec_bpe.sh
+++ scripts/whale/ft_data2vec_bpe.sh
@@ -8,7 +8,8 @@ fairseq-hydra-train \
 	task.data=/dev/shm/manifests \
 	model.w2v_path=/home/work/workspace/models/wav2vec_model/wav2vec_small.pt \
 	checkpoint.save_dir=/home/work/workspace/fairseq/scripts/whale/outputs/$1 \
-	criterion._name=bpe 
+	criterion._name=bpe \
+	+criterion.lm_decay=1.0
 
 #common.wandb_project=lm2am_distill \
 : <<'END'
