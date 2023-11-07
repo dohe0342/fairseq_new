949e6fd9204c3a41e97817ca1b6203ad8ee637ac
dohe0342 <kimdohe1070@gmail.com>
2023-11-07 14:12:47 +0900

    from local

--- scripts/whale/ft_data2vec_prompt.sh
+++ scripts/whale/ft_data2vec_prompt.sh
@@ -9,6 +9,5 @@ fairseq-hydra-train \
 	task.normalize=true \
 	model.w2v_path=/home/work/workspace/models/data2vec_model/audio_base_ls.pt \
 	checkpoint.save_dir=/home/work/workspace/fairseq/scripts/whale/outputs/$1 \
-	dataset.train_subset=ted2_train \
-	dataset.valid_subset=ted2_dev \
-	+task.min_sample_size=32000
+	dataset.train_subset=train-100 \
+	dataset.valid_subset=dev-other \
