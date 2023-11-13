72954d1f1294b7e5e4380311ecebb5a84aa3ba2a
DoheeKim <dohe0342@DoheeKims-MacBook-Pro.local>
2023-11-13 15:07:38 +0900

    from local

--- fairseq/criterions/ctc.py
+++ fairseq/criterions/ctc.py
@@ -2742,13 +2742,13 @@ class Clip3Criterion(FairseqCriterion):
         alignment_lengths = torch.sum(lm_input["attention_mask"], 1)
 
         alignment_flat = torch.linspace(
-                                            1, 
+                                            2, 
                                             alignment_lengths[0], 
                                             steps=alignment_lengths[0]
                                     ).to(device)
         
         for i in alignment_lengths[1:]:
-            temp_target = torch.linspace(1, i, steps=i).to(device)
+            temp_target = torch.linspace(2, i, steps=i).to(device)
             alignment_flat = torch.cat([alignment_flat, temp_target])
             alignment_flat = alignment_flat.to(torch.cuda.IntTensor())
         #############for alignment target ###############################
