f31bd852d8be4c2c052b9f9e5fc12d187360421a
김도희 <dohee@macmini.local>
2023-11-16 00:23:03 +0900

    from local

--- scripts/whale/ft_data2vec_clip.sh
+++ scripts/whale/ft_data2vec_clip.sh
@@ -11,7 +11,8 @@ fairseq-hydra-train \
 	checkpoint.save_dir=/home/work/workspace/fairseq/scripts/whale/outputs/$1 \
 	criterion._name=clip3 \
 	+criterion.decoder=conv \
-	+criterion.lm=gpt2
+	+criterion.lm=gpt2 \
+	+criterion.lm_decay=0.2
 
 : <<'END'
 fairseq-hydra-train \
