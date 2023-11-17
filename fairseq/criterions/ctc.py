589083097d6c9b0cb6a15c908a1dbd0a2b0f1d41
DoheeKim <dohe0342@DoheeKims-MacBook-Pro.local>
2023-11-17 17:03:59 +0900

    from local

--- fairseq/criterions/ctc.py
+++ fairseq/criterions/ctc.py
@@ -2659,7 +2659,7 @@ class Clip3Criterion(FairseqCriterion):
                 am_output = F.normalize(am_output, dim=2)
                 
                 lm_am_sim = torch.bmm(am_output, lm_output.transpose(1, 2))
-                lm_am_sim *= 100.
+                lm_am_sim *= 500.
                 
             if 0:
                 #lm_output = F.normalize(lm_output, dim=2)
