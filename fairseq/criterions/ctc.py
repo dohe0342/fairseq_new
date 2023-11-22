2f41f7ba95f6c2b89a870af0c6edc058cf40a2c7
김도희 <dohee@macmini.local>
2023-11-22 18:00:17 +0900

    from local

--- fairseq/criterions/ctc.py
+++ fairseq/criterions/ctc.py
@@ -3154,7 +3154,7 @@ class L2SCriterion(FairseqCriterion):
                 zero_infinity=self.zero_infinity,
             )
 
-            distill_loss = nn.functional.mse_loss(am_output_shrink, lm_output, reduction='none')
+            distill_loss = F.mse_loss(am_output_shrink, lm_output, reduction='none')
             distill_loss = distill_loss[am_output_pad_mask]
             distill_loss = sum(distill_loss)
             print(distill_loss)
