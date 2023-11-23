2b4e8b8a07ced80bc70d4abf519f3e8b386a82fc
DoheeKim <dohe0342@DoheeKims-MacBook-Pro.local>
2023-11-23 12:21:07 +0900

    from local

--- fairseq/criterions/ctc.py
+++ fairseq/criterions/ctc.py
@@ -3158,8 +3158,9 @@ class L2SCriterion(FairseqCriterion):
             distill_loss = F.mse_loss(am_output_shrink, lm_output, reduction='none')
             loss_time1 = time.time() - loss_time
 
-            loss_time
+            loss_time2 = time.time()
             distill_loss = distill_loss[am_output_pad_mask]
+            loss_time2 = time.time() - loss_time2
             distill_loss = sum(distill_loss)
 
             print(shrink, inter, loss_time)
