85c6219b042617d2ed8d43af78e6c3a275b02f54
DoheeKim <dohe0342@DoheeKims-MacBook-Pro.local>
2023-11-23 11:55:12 +0900

    from local

--- fairseq/criterions/ctc.py
+++ fairseq/criterions/ctc.py
@@ -3061,7 +3061,7 @@ class L2SCriterion(FairseqCriterion):
         
             elif self.decoder_type == 'transf_enc':
                 am_output = self.lm_decoder(am_output, padding_mask)
-
+            am_output = self.lm_decoder(am_output)
             #am_output = am_output.transpose(1, 2)
             
             if 1:
