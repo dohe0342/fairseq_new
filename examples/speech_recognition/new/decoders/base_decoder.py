c14be258db1bed245140e3602be70574e330c017
김도희 <dohee@dohee.local>
2023-12-08 16:32:20 +0900

    from local

--- examples/speech_recognition/new/decoders/base_decoder.py
+++ examples/speech_recognition/new/decoders/base_decoder.py
@@ -48,6 +48,9 @@ class BaseDecoder:
         print(encoder_out['encoder_out'][0][0].size(), encoder_out['encoder_out'][0][0])
         print(encoder_out['encoder_feat'][0][1].size(), encoder_out['encoder_feat'][0][1])
         print(encoder_out['encoder_out'][0][1].size(), encoder_out['encoder_out'][0][1])
+        print(encoder_out['encoder_feat'][0][5].size(), encoder_out['encoder_feat'][0][5])
+        print(encoder_out['encoder_out'][0][5].size(), encoder_out['encoder_out'][0][5])
+
         exit()
         if hasattr(model, "get_logits"):
             emissions = model.get_logits(encoder_out)
