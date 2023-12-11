01403d7f1a2083c022fae5eafa54c8882eaa9b3a
김도희 <dohee@dohee.local>
2023-12-11 16:06:07 +0900

    from local

--- fairseq/models/wav2vec/wav2vec2.py
+++ fairseq/models/wav2vec/wav2vec2.py
@@ -1801,6 +1801,188 @@ class TransformerSentenceEncoderLayer(nn.Module):
         return x, (attn, layer_result)
 
 
+class TransformerSentenceEncoderLayerForDistill(nn.Module):
+    """
+    Implements a Transformer Encoder Layer used in BERT/XLM style pre-trained
+    models.
+    """
+
+    def __init__(
+        self,
+        embedding_dim: float = 768,
+        ffn_embedding_dim: float = 3072,
+        num_attention_heads: int = 8,
+        dropout: float = 0.1,
+        attention_dropout: float = 0.1,
+        activation_dropout: float = 0.1,
+        activation_fn: str = "relu",
+        layer_norm_first: bool = False,
+    ) -> None:
+
+        super().__init__()
+        # Initialize parameters
+        self.embedding_dim = embedding_dim
+        self.dropout = dropout
+        self.activation_dropout = activation_dropout
+
+        # Initialize blocks
+        self.activation_fn = utils.get_activation_fn(activation_fn)
+        self.self_attn = MultiheadAttention(
+            self.embedding_dim,
+            num_attention_heads,
+            dropout=attention_dropout,
+            self_attention=True,
+        )
+
+        self.dropout1 = nn.Dropout(dropout)
+        self.dropout2 = nn.Dropout(self.activation_dropout)
+        self.dropout3 = nn.Dropout(dropout)
+
+        self.layer_norm_first = layer_norm_first
+
+        # layer norm associated with the self attention layer
+        self.self_attn_layer_norm = LayerNorm(self.embedding_dim)
+        self.fc1 = nn.Linear(self.embedding_dim, ffn_embedding_dim)
+        self.fc2 = nn.Linear(ffn_embedding_dim, self.embedding_dim)
+
+        # layer norm associated with the position wise feed-forward NN
+        self.final_layer_norm = LayerNorm(self.embedding_dim)
+
+    def forward(
+        self,
+        x: torch.Tensor,
+        self_attn_mask: torch.Tensor = None,
+        self_attn_padding_mask: torch.Tensor = None,
+        need_weights: bool = False,
+        att_args=None,
+        prefix=None,
+    ):
+        """
+        LayerNorm is applied either before or after the self-attention/ffn
+        modules similar to the original Transformer imlementation.
+        """
+        residual = x
+
+        if prefix is not None:
+            key_prefix = prefix[0].to(x.device)
+            value_prefix = prefix[1].to(x.device)
+
+        if self.layer_norm_first:
+            if prefix is None:
+                x = self.self_attn_layer_norm(x)
+                x, attn = self.self_attn(
+                    query=x,
+                    key=x,
+                    value=x,
+                    key_padding_mask=self_attn_padding_mask,
+                    attn_mask=self_attn_mask,
+                    need_weights=False,
+                )
+                x = self.dropout1(x)
+                x = residual + x
+
+                residual = x
+                x = self.final_layer_norm(x)
+                x = self.activation_fn(self.fc1(x))
+                x = self.dropout2(x)
+                x = self.fc2(x)
+
+                layer_result = x
+
+                x = self.dropout3(x)
+                x = residual + x
+            
+            else:
+                x = self.self_attn_layer_norm(x)
+                x, attn = self.self_attn(
+                    query=x,
+                    key=x,
+                    value=x,
+                    key_padding_mask=self_attn_padding_mask,
+                    attn_mask=self_attn_mask,
+                    need_weights=False,
+                )
+                x = self.dropout1(x)
+                x = residual + x
+
+                residual = x
+                x = self.final_layer_norm(x)
+                x = self.activation_fn(self.fc1(x))
+                x = self.dropout2(x)
+                x = self.fc2(x)
+
+                layer_result = x
+
+                x = self.dropout3(x)
+                x = residual + x
+
+        else:
+            if prefix is None:
+                x, attn = self.self_attn(
+                    query=x,
+                    key=x,
+                    value=x,
+                    key_padding_mask=self_attn_padding_mask,
+                    need_weights=False,
+                )
+
+                x = self.dropout1(x)
+                x = residual + x
+
+                x = self.self_attn_layer_norm(x)
+
+                residual = x
+                x = self.activation_fn(self.fc1(x))
+                x = self.dropout2(x)
+                x = self.fc2(x)
+
+                layer_result = x
+
+                x = self.dropout3(x)
+                x = residual + x
+                x = self.final_layer_norm(x)
+            
+            else:
+                key_prefix = key_prefix.expand((x.size()[1], key_prefix.size()[0], key_prefix.size()[1]))
+                value_prefix = value_prefix.expand((x.size()[1], value_prefix.size()[0], value_prefix.size()[1]))
+                #print('*'*20)
+                #print('x', x.size())
+                #print('key', x.size())
+                #print('value', x.size())
+                #print('*'*20)
+                #key = torch.cat([key_prefix, x], dim=1)
+                #value = torch.cat([value_prefix, x], dim=1)
+
+                x, attn = self.self_attn(
+                    query=x,
+                    key=x,
+                    value=x,
+                    key_padding_mask=self_attn_padding_mask,
+                    need_weights=False,
+                    prefix=[key_prefix, value_prefix],
+                )
+
+                x = self.dropout1(x)
+                x = residual + x
+
+                x = self.self_attn_layer_norm(x)
+
+                residual = x
+                x = self.activation_fn(self.fc1(x))
+                x = self.dropout2(x)
+                x = self.fc2(x)
+
+                layer_result = x
+
+                x = self.dropout3(x)
+                x = residual + x
+                x = self.final_layer_norm(x)
+
+
+        return x, (attn, layer_result)
+
+
+
 class AdapterFast(nn.Module):
     def __init__(self, adapter_num, input_dim, hidden_dim, act_fn):
         """
