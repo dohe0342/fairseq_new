4d6230c2da1268dc69c6d8b8bfb40154605bb000
김도희 <dohee@dohee.local>
2024-01-03 16:51:02 +0900

    from local

--- examples/speech_recognition/new/infer.py
+++ examples/speech_recognition/new/infer.py
@@ -18,6 +18,7 @@ from typing import Any, Dict, List, Optional, Tuple, Union
 import editdistance
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import torch.distributed as dist
 from examples.speech_recognition.new.decoders.decoder_config import (
     DecoderConfig,
