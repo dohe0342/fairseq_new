97114822c5abf3865c1c451cf7bccfbebe0a825a
김도희 <dohee@dohee.local>
2024-01-03 16:45:34 +0900

    from local

--- examples/speech_recognition/new/infer.py
+++ examples/speech_recognition/new/infer.py
@@ -112,9 +112,74 @@ class InferenceProcessor:
         else:
             self.prompt = None
         #self.prompt = None
+        
+        if 1:
+            conv_layers = [(768, 5, 2)] * 3
+            mode = "layer_norm"
+            dropout = 0.0
 
+            def block(
+                n_in,
+                n_out,
+                k,
+                stride,
+                groups=1,
+                is_layer_norm=False,
+                is_group_norm=False,
+                conv_bias=False,
+            ):
+                def make_conv():
+                    conv = nn.Conv1d(n_in, n_out, k, stride=stride, bias=conv_bias, groups=groups)
+                    nn.init.kaiming_normal_(conv.weight)
+                    return conv
+
+                assert (
+                    is_layer_norm and is_group_norm
+                ) == False, "layer norm and group norm are exclusive"
+
+                if is_layer_norm:
+                    return nn.Sequential(
+                        make_conv(),
+                        nn.Dropout(p=dropout),
+                        nn.Sequential(
+                            TransposeLast(),
+                            Fp32LayerNorm(dim, elementwise_affine=True),
+                            TransposeLast(),
+                        ),
+                        nn.GELU(),
+                    )
+                elif is_group_norm:
+                    return nn.Sequential(
+                        make_conv(),
+                        nn.Dropout(p=dropout),
+                        Fp32GroupNorm(dim, dim, affine=True),
+                        nn.GELU(),
+                    )
+                else:
+                    return nn.Sequential(make_conv(), nn.Dropout(p=dropout), nn.GELU())
+                
+            self.lm_decoder = nn.ModuleList()
+            for i, cl in enumerate(conv_layers):
+                assert len(cl) == 3, "invalid conv definition: " + str(cl)
+                (dim, k, stride) = cl 
+
+                self.lm_decoder.append(
+                    block(
+                        dim,
+                        dim,
+                        k,
+                        stride,
+                        is_layer_norm=(mode == "layer_norm"),
+                        is_group_norm=(mode == "default") and i == 0,
+                        conv_bias=False,
+                    )
+                )
+            
+            self.lm_decoder.load_state_dict(cri, strict=False)
+        else:
+            self.lm_decoder = None
+        
         del ckpt, criterion
-
         ### LOAD ADAPTER ####
         ckpt_obj = checkpoint_utils.load_checkpoint_to_cpu(self.cfg.common_eval.path)
         if "adapter" in ckpt_obj:
