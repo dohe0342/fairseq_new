83cdeb5433ed8f28d3a843be5b1abc63d8fb6fa5
김도희 <dohee@dohee.local>
2024-01-03 17:09:20 +0900

    from local

--- examples/speech_recognition/new/infer.py
+++ examples/speech_recognition/new/infer.py
@@ -430,7 +430,7 @@ class InferenceProcessor:
             sample=sample,
         )
 
-        encoder_out = self.models[0](**sample["net_input"])
+        net_output = self.models[0](**sample["net_input"])
         am_output = net_output['encoder_feat'].transpose(0, 1) ## T x B x C -> B x T x C
         am_output = am_output.transpose(1, 2).contiguous()
         for i, conv in enumerate(self.lm_decoder):
