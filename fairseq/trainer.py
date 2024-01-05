67fd27278537629617bbb9bd11f0ea399b1c30de
김도희 <dohee@dohee.local>
2024-01-05 14:10:46 +0900

    from local

--- fairseq/trainer.py
+++ fairseq/trainer.py
@@ -305,8 +305,7 @@ class Trainer(object):
                     p.requires_grad = False
         if 1:
             for n, p in self.criterion.named_parameters():
-                print(n)
-                if 'mistral' in n:
+                if 'lm.' in n:
                     p.requires_grad = False
                 else:
                     p.requires_grad = True
