26d63012665ccb0523aedd818a03979e3ed8d964
김도희 <dohee@dohee.local>
2024-01-05 10:30:39 +0900

    from local

--- examples/speech_recognition/new/infer.py
+++ examples/speech_recognition/new/infer.py
@@ -466,7 +466,7 @@ class InferenceProcessor:
             sim6 = torch.matmul(dim1, dim7)
             sim7 = torch.matmul(dim1, dim8)
 
-            print(am_output.size(), sim1.item(), sim2.item(), sim3.item(), sim4.item(), sim5.item(), sim6.item(), sim7.item())
+            print(am_output.size(), dim1.size(), sim1.item(), sim2.item(), sim3.item(), sim4.item(), sim5.item(), sim6.item(), sim7.item())
 
             exit()
 
