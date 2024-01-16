3ec97429c9f16faf09ce3cd43fd551a354cca318
DoheeKim <dohe0342@DoheeKims-MacBook-Pro.local>
2024-01-17 02:40:53 +0900

    from local

--- scripts/whale/test.py
+++ scripts/whale/test.py
@@ -6,9 +6,10 @@ import matplotlib as plt
 a = [torch.randn(642, 96) for i in range(8)]
 b = [torch.randn(56, 96) for i in range(8)]
 
-lm_am_sim_cp = [F.softmax(torch.matmul(a[i], b[i].T)/3, dim=0) for i in range(8)]
+lm_am_sim_cp = [F.softmax(torch.matmul(a[i], b[i].T)/3, dim=1) for i in range(8)]
 lm_am_sim = None
 
+'''
 for i in range(8):
     print(lm_am_sim_cp[i].size())
     if i == 0:
@@ -18,11 +19,12 @@ for i in range(8):
 
 print(lm_am_sim.size())
 exit()
-    
+'''
 
-plt.matshow(lm_am_sim_cp.numpy())
-plt.colorbar()
-if not os.path.exists(f'/home/work/workspace/fairseq/scripts/whale/png/cross_attn'):
-    try: os.makedirs(f'/home/work/workspace/fairseq/scripts/whale/png/cross_attn')
-    except: pass
-plt.savefig(f'/home/work/workspace/fairseq/scripts/whale/png/aross_attn/cross_attn.png')
+for i in range(8):
+    plt.matshow(lm_am_sim_cp[i].numpy())
+    plt.colorbar()
+    if not os.path.exists(f'/home/work/workspace/fairseq/scripts/whale/png/cross_attn'):
+        try: os.makedirs(f'/home/work/workspace/fairseq/scripts/whale/png/cross_attn')
+        except: pass
+    plt.savefig(f'/home/work/workspace/fairseq/scripts/whale/png/aross_attn/cross_attn_{i}.png')
