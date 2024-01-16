1e82771167678df24ccd0e5a2a23e659073469fd
DoheeKim <dohe0342@DoheeKims-MacBook-Pro.local>
2024-01-17 02:38:25 +0900

    from local

--- scripts/whale/test.py
+++ scripts/whale/test.py
@@ -6,7 +6,7 @@ import matplotlib as plt
 a = [torch.randn(642, 96) for i in range(8)]
 b = [torch.randn(56, 96) for i in range(8)]
 
-lm_am_sim_cp = [F.Softmax(torch.matmul(a[i], b[i].T)/3, dim=0) for i in range(8)]
+lm_am_sim_cp = [F.softmax(torch.matmul(a[i], b[i].T)/3, dim=0) for i in range(8)]
 lm_am_sim = None
 
 for i in range(8):
