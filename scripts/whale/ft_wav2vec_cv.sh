6aecc980898d7255c56b9aa86d49e85def0c75b9
DoheeKim <dohe0342@DoheeKims-MacBook-Pro.local>
2024-02-04 23:57:55 +0900

    from local

--- scripts/whale/ft_wav2vec_cv.sh
+++ scripts/whale/ft_wav2vec_cv.sh
@@ -7,7 +7,7 @@ for lang in "de"; do
 		--config-name base_100h \
 		common.user_dir=examples/data2vec \
 		common.wandb_project=sample_reweight \
-		common.log_file=/workspace/fairseq/scripts/whale/outputs/$lang.log \
+		common.log_file=/home/work/workspace/fairseq/scripts/whale/outputs/$lang.log \
 		task.data=/home/work/workspace/models/manifests \
 		model.w2v_path=/home/work/workspace/models/wav2vec_model/$model_name \
 		checkpoint.save_dir=/home/work/workspace/fairseq/scripts/whale/outputs/$exp_name \
