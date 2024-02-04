97c46e284a04849a09719ebafbb4c5b8b05bc0c5
DoheeKim <dohe0342@DoheeKims-MacBook-Pro.local>
2024-02-05 00:33:58 +0900

    from local

--- scripts/whale/ft_wav2vec_cv.sh
+++ scripts/whale/ft_wav2vec_cv.sh
@@ -8,7 +8,7 @@ for lang in "es" "de"; do
 		common.user_dir=examples/data2vec \
 		common.wandb_project=sample_reweight \
 		common.log_file=/home/work/workspace/fairseq/scripts/whale/outputs/$lang.log \
-		task.data=/home/work/workspace/LibriSpeech/manifests \
+		task.data=/home/work/workspace/LibriSpeech/manifests/cv_$lang \
 		model.w2v_path=/home/work/workspace/models/wav2vec_model/$model_name \
 		checkpoint.save_dir=/home/work/workspace/fairseq/scripts/whale/outputs/$exp_name \
 		dataset.train_subset=cv5.1_"$lang"_train \
