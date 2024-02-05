493409748075582d272f2d03cae85c3984c5bbf6
김도희 <dohee@dohee.local>
2024-02-06 01:52:41 +0900

    from local

--- scripts/whale/ft_wav2vec_cv.sh
+++ scripts/whale/ft_wav2vec_cv.sh
@@ -1,26 +1,24 @@
 #exp_name=$1
 model_name=$1
-language=$2
-for lang in $language; do
-	exp_name="$model_name"_"$lang"
-	fairseq-hydra-train \
-		--config-dir /home/work/workspace/fairseq/examples/wav2vec/config/finetuning \
-		--config-name base_100h \
-		common.user_dir=examples/data2vec \
-		common.wandb_project=sample_reweight \
-		common.log_file=/home/work/workspace/fairseq/scripts/whale/outputs/$lang.log \
-		task.data=/home/work/workspace/LibriSpeech/manifests/cv_$lang \
-		model.w2v_path=/home/work/workspace/models/wav2vec_model/$model_name \
-		checkpoint.save_dir=/home/work/workspace/fairseq/scripts/whale/outputs/$exp_name \
-		dataset.train_subset=cv5.1_"$lang"_train \
-		dataset.valid_subset=cv5.1_"$lang"_dev \
-		dataset.max_tokens=3200000 \
-		optimization.update_freq=[2] \
-		optimization.lr=[0.00006] \
-		criterion._name=ctc \
-		distributed_training.distributed_world_size=4 \
-		+task.min_sample_size=16000
-done
+lang=$2
+exp_name="$model_name"_"$lang"
+fairseq-hydra-train \
+	--config-dir /home/work/workspace/fairseq/examples/wav2vec/config/finetuning \
+	--config-name base_100h \
+	common.user_dir=examples/data2vec \
+	common.wandb_project=sample_reweight \
+	common.log_file=/home/work/workspace/fairseq/scripts/whale/outputs/$lang.log \
+	task.data=/home/work/workspace/LibriSpeech/manifests/cv_$lang \
+	model.w2v_path=/home/work/workspace/models/wav2vec_model/$model_name \
+	checkpoint.save_dir=/home/work/workspace/fairseq/scripts/whale/outputs/$exp_name \
+	dataset.train_subset=cv5.1_"$lang"_train \
+	dataset.valid_subset=cv5.1_"$lang"_dev \
+	dataset.max_tokens=3200000 \
+	optimization.update_freq=[2] \
+	optimization.lr=[0.00006] \
+	criterion._name=ctc \
+	distributed_training.distributed_world_size=4 \
+	+task.min_sample_size=16000
 
 #task.data=/home/work/workspace/LibriSpeech/manifests \
 : <<'END'
