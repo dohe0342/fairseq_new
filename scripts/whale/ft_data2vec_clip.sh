e48dfed6fb85d9f495476c56f1e99572b3adf2d1
DoheeKim <dohe0342@DoheeKims-MacBook-Pro.local>
2024-02-13 11:03:53 +0900

    from local

--- scripts/whale/ft_data2vec_clip.sh
+++ scripts/whale/ft_data2vec_clip.sh
@@ -4,7 +4,7 @@ fairseq-hydra-train \
 	common.log_file=/home/work/workspace/fairseq/scripts/whale/outputs/$1.log \
 	distributed_training.distributed_world_size=4 \
 	dataset.max_tokens=3200000 \
-	dataset.train_subset=train-100 \
+	dataset.train_subset=train-1960 \
 	dataset.valid_subset=dev-other \
 	optimization.update_freq=[2] \
 	task.data=/dev/shm/manifests \
