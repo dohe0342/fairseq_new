2d6966d50ef961510669f03c9d8cfe28b6e52577
김도희 <dohee@dohee.local>
2024-02-25 20:27:41 +0900

    from local

--- examples/speech_recognition/new/infer.py
+++ examples/speech_recognition/new/infer.py
@@ -202,6 +202,7 @@ class InferenceProcessor:
             self.lm_decoder = self.lm_decoder.to('cuda')
             print(self.lm_decoder)
             self.lm_decoder.load_state_dict(criterion, strict=False)
+            print(self.lm_decoder)
             self.tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
             self.tokenizer.pad_token = self.tokenizer.eos_token
             self.lm = GPT2Model.from_pretrained('gpt2').eval()
