39a8877201d30bc130ee4f4019854f4d0d8d66a4
김도희 <dohee@dohee.local>
2024-02-25 20:12:17 +0900

    from local

--- examples/speech_recognition/new/infer.py
+++ examples/speech_recognition/new/infer.py
@@ -458,7 +458,7 @@ class InferenceProcessor:
             am_output = F.normalize(am_output, dim=2)
             lm_output = F.normalize(lm_output, dim=2)
 
-            lm_am_sim = 200* torch.bmm(am_output, lm_output.transpose(1, 2))
+            lm_am_sim = 300 * torch.bmm(am_output, lm_output.transpose(1, 2))
             lm_am_sim = F.softmax(lm_am_sim, dim=-1)
 
         for b in range(lm_am_sim.size(0)):
