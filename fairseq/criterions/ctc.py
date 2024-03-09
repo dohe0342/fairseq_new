59af4c7fab2697159c2f5907e2f5685511dd7e31
김도희 <dohee@dohee.local>
2024-03-10 02:20:10 +0900

    from local

--- fairseq/criterions/ctc.py
+++ fairseq/criterions/ctc.py
@@ -3183,7 +3183,7 @@ class Clip3Criterion(FairseqCriterion):
 
             am_output = am_output.transpose(1, 2)
             if self.final_linear is not None:
-                lm_output = F.normalize(lm_output, dim=2)
+                #lm_output = F.normalize(lm_output, dim=2)
                 #am_output = self.final_linear(am_output)
                 lm_output = self.final_linear(lm_output)
             
